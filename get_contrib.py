6e6fafb6330cebb11cef050f106400d0cab760f9
alex-hancock <alexbhancock@gmail.com>
2018-05-16 15:23:29 -0700

    Moved experiment to get_contrib. Added input for username and GitHub credentials (password-protected)

--- get_contrib.py
+++ get_contrib.py
@@ -1,26 +1,19 @@
 import grequests as async
 import requests
 import datetime
+import sys
+import getpass
 
-def async_retrieve(repo_url_list, suffix):
-    amended_url_list = [url + suffix for url in repo_url_list]
-    async_requests = (async.get(url, stream=False) for url in amended_url_list)
-    return async.map(async_requests)
+from requests.auth import HTTPBasicAuth
+username = input("GitHub username:")
+password = getpass.getpass("GitHub password: ")
+GITHUB_AUTH = HTTPBasicAuth('alex-hancock', password)
 
-def create_daylist(since_date,today_date):
-    today_obj = datetime.date(int(today_date[0:4]), int(today_date[5:7]), int(today_date[-2:]))
-    since_obj = datetime.date(int(since_date[0:4]), int(since_date[5:7]), int(since_date[-2:]))
-
-    delta = today_obj - since_obj
-    thing = [since_obj + datetime.timedelta(days=i) for i in range(delta.days+1)]
-    return [x.strftime("%Y-%m-%d") for x in thing]
+USER = input("Please enter the username you'd like to search for: ")
 
 today_date = datetime.date.today().isoformat()
 since_date = str(int(today_date[0:4])-1) + today_date[4:]
 days_hash = {day: 0 for day in create_daylist(since_date, today_date)}
-
-# Change how USER is passed at some point...
-USER = "jvivian"
 
 #
 # Initialize URLs
@@ -30,62 +23,89 @@ REPOS_URL  = "/users/" + USER + "/repos?type=all"
 ORG_URL = "/users/" + USER + "/orgs"
 repo_url_list = []
 
+def add_repo(repo_dict):
+    # dictionary checking done in retry section
+    repo_url_list.append(repo_dict["url"])
+
+def issue_increment_days_hash(issue_dict):
+    issue_user = issue_dict["user"]["login"]
+    created_date = issue_dict["created_at"][:10]
+    if created_date in days_hash and issue_user == USER:
+        days_hash[created_date] += 1
+
+def commit_increment_days_hash(commit_dict):
+    commit_user = commit_dict["author"]["login"]
+    created_date = commit_dict["commit"]["author"]["date"][:10]
+    if created_date in days_hash and commit_user == USER:
+        days_hash[created_date] += 1
+
+def requests_with_retry(desired_url, dictionary_function):
+    request_obj = requests.get(desired_url, auth=GITHUB_AUTH)
+
+    if request_obj.status_code != 200:
+        if request_obj.status_code == 409:
+            # Encounter when repository is empty
+            return
+        else:
+            print("Failure during request to {}".format(desired_url))
+            print("Returned: {}".format(request_obj.json()))
+            sys.exit(1)
+
+    for request_dict in request_obj.json():
+        dictionary_function(request_dict)
+
+    if "Link" in request_obj.headers.keys():
+        page_num = 2
+        while "next" in request_obj.headers["Link"]:
+            page_suffix = "&page=" if "?" in desired_url else "?page="
+            request_obj = requests.get(desired_url + page_suffix + str(page_num), auth=GITHUB_AUTH)
+            page_num += 1
+            for request_dict in request_obj.json():
+                dictionary_function(request_dict)
+
+def create_daylist(since_date,today_date):
+    today_obj = datetime.date(int(today_date[0:4]), int(today_date[5:7]), int(today_date[-2:]))
+    since_obj = datetime.date(int(since_date[0:4]), int(since_date[5:7]), int(since_date[-2:]))
+
+    delta = today_obj - since_obj
+    thing = [since_obj + datetime.timedelta(days=i) for i in range(delta.days+1)]
+    return [x.strftime("%Y-%m-%d") for x in thing]
+
 #
 # User owned/member repo request
 #
-repo_request = requests.get(GITHUB_URL + REPOS_URL)
-for repo_dict in repo_request.json():
-    # Comment for sublime collapse
-    repo_url_list.append(repo_dict["url"])
-if "Link" in repo_request.headers:
-    page_num = 2
-    while "next" in repo_request.headers["Link"]:
-        repo_request = requests.get(GITHUB_URL + REPOS_URL + "&page=" + str(page_num))
-        page_num += 1
-        for repo_dict in repo_request.json():
-            repo_url_list.append(repo_dict["url"])
+print("Checking repositories of which", USER, "is either an owner or member...")
+requests_with_retry(GITHUB_URL + REPOS_URL, add_repo)
+print("Done!")
 
 #
 # Organizations they belong to, list their repos
 #
-org_request = requests.get(GITHUB_URL + ORG_URL)
+print("Checking repositories of", USER, "organizations...")
+org_request = requests.get(GITHUB_URL + ORG_URL, auth=GITHUB_AUTH)
 for org_dict in org_request.json():
-    org_repos_url = org_dict["repos_url"]
-    org_repo_request = requests.get(org_repos_url)
-    for repo_dict in org_repo_request.json():
-        repo_url_list.append(repo_dict["url"])
+    requests_with_retry(org_dict["repos_url"], add_repo)
+print("Done!")
 
-    if "Link" in org_repo_request.headers:
-        page_num = 2
-        while "next" in org_repo_request.headers["Link"]:
-            org_repo_request = requests.get(org_repos_url + "?page=" + str(page_num))
-            page_num += 1
-            for repo_dict in org_repo_request.json():
-                repo_url_list.append(repo_dict["url"])
-
-
+#
+# Collect issues
 # 
+print("Accounting for issues/pull requests...")
 issue_suffix = "/issues?state=all&since=" + since_date
 #issue_results = async_retrieve(repo_url_list, issue_suffix)
 for repo_url in repo_url_list:
-    # for each repo url, get repos issues
-    issue_request = requests.get(repo_url + issue_suffix)
-    for issue_dict in issue_request.json():
-        issue_user = issue_dict["user"]["login"]
-        created_date = issue_dict["created_at"][:10]
-        print("{} created an issue on {}".format(issue_user, created_date))
-        
+    requests_with_retry(repo_url + issue_suffix, issue_increment_days_hash)
+print("Done!")
 
-for issue_request in issue_results:
-    print(issue_request.headers)
-    break
-
-import sys
-sys.exit(0)
-
-# by the API, pull requests SHOULD be integrated as issues, so we don't care about these
-
-# get commits since date with USER as author
-# https://developer.github.com/v3/repos/commits/#get-a-single-commit
-commit_suffix = ""
+#
+# Collect commits
+#
+print("Accounting for commits...")
+commit_suffix = "/commits?author=" + USER + "&since=" + since_date
 #commit_results = async_retrieve(repo_url_list, commit_suffix)
+for repo_url in repo_url_list:
+    requests_with_retry(repo_url + commit_suffix, commit_increment_days_hash)
+print("Done!")
+
+contribution_list = days_hash.values()
+print(contribution_list)
